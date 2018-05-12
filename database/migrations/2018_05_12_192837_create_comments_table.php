d8190cd154c73282a80d82939cd11ccbd277d64c
katatectic <katatectic@gmail.com>
2018-05-12 22:34:24 +0300

    reports, comments migrations

---
+++ database/migrations/2018_05_12_192837_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('news_id')->unsigned();
+            $table->foreign('news_id')->references('id')->on('news');
+            $table->integer('report_id')->unsigned();
+            $table->foreign('report_id')->references('id')->on('reports');            
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
