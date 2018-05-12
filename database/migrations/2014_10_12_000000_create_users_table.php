f26fabbbacaca63a1d8719bbe46426b015153fb7
platinize <you@example.com>
2018-05-12 18:18:20 +0300

    migrations: user, create studentClass, user_studentClass. changed the character set.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,19 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table)
+        {
             $table->increments('id');
+            $table->string('surname');
             $table->string('name');
+            $table->string('middle_name');
             $table->string('email')->unique();
+            $table->integer('phone');
             $table->string('password');
+            $table->tinyInteger('role')->default(4);
+            $table->string('appointment', 100)->comment('Должность внутри комитета');
+            $table->string('your_child', 120)->comment('ФИО ребёнка');
+            $table->string('avatar')->default('storage/app/avatars/default_avatar.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2018_05_12_111209_create_studentClass_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentClassTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studentClass', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('letter_class', 2)->comment('литера класса');
+            $table->smallInteger('start_year')->lenght(4)->comment('первый учебный год');
+            $table->smallInteger('year_of_issue')->lenght(4)->comment('год выпуска');
+            $table->boolean('fourth_class')->default(0)->comment('включён 4 класс');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studentClass');
+    }
+}
