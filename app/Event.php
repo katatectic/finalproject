32006fb79413ce180345d5ebdd62528779307590
katatectic <katatectic@gmail.com>
2018-05-14 15:20:28 +0300

    Little change

--- app/Event.php
+++ app/Event.php
@@ -6,12 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-<<<<<<< HEAD
-	public $fillable = ['title','event_date','event_hours', 'address','description','content','photo'];
 
-=======
+
+
 	public $fillable = ['title','author_id','event_date','event_hours', 'address','description','content','photo'];
->>>>>>> a67205426090817f2792af0d7600fdca5855935d
     public function user() {
         return $this->belongsTo('App\User');
     }
