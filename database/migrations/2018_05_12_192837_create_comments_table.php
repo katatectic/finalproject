6a9ebffec87ab8282703acdc94cd9fbeb4c6ac8f
devilchonok <devilchonok@gmail.com>
2018-05-29 11:46:28 +0300

    migration

--- database/migrations/2018_05_12_192837_create_comments_table.php
+++ database/migrations/2018_05_12_192837_create_comments_table.php
@@ -16,13 +16,13 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('article_id')->unsigned()->nullable();
-            $table->foreign('article_id')->references('id')->on('news');
+            $table->foreign('article_id')->references('id')->on('news')->onDelete('cascade');
             $table->integer('event_id')->unsigned()->nullable();
-            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->integer('report_id')->unsigned()->nullable();
-            $table->foreign('report_id')->references('id')->on('reports');     
+            $table->foreign('report_id')->references('id')->on('reports')->onDelete('cascade');     
             $table->string('comment');
             $table->tinyInteger('ispublished')->default(0);
             $table->timestamps();
