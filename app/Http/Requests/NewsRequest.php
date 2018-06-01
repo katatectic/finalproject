28d722e3711b376369c9aa4e18d0bd5fa0271597
platinize <you@example.com>
2018-06-01 16:11:26 +0300

    edit NewsRequest for validation user->studentsClasses

--- app/Http/Requests/NewsRequest.php
+++ app/Http/Requests/NewsRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class NewsRequest extends FormRequest {
 
@@ -21,20 +23,24 @@ class NewsRequest extends FormRequest {
      * @return array
      */
     public function rules() {
+        $userId = Auth::id();
+        $user = User::with('studentsClasses')->find($userId);
+        $studentsClasses = $user->studentsClasses->keyBy('id')->keys();
         return [
             'title' => 'required',
             'date' => 'required',
             'content' => 'required',
             'photo' => 'required|image|max:2048',
             'description' => 'required',
-			'student_class_id'=>'required'];
+			'student_class_id'=>'required|in:'.$studentsClasses.', 0'];
     }
 
     public function messages() {
         return [
             '*.required' => 'Поле не должно быть пустым',
             'photo.image' => 'Загруженный файл должен быть изображением',
-            'photo.max' => 'Максимальный размер изображения=2048'
+            'photo.max' => 'Максимальный размер изображения=2048',
+            'student_class_id.in' => 'Вы не состоите в этом комитете'
         ];
     }
 
