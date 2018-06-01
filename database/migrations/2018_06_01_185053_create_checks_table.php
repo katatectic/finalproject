42dd4599dbaa80e226bc6bd6ebcd6cb57f38cf44
platinize <you@example.com>
2018-06-01 22:17:49 +0300

    add migration checks

---
+++ database/migrations/2018_06_01_185053_create_checks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('report_id')->unsigned()->nullable();
+            $table->foreign('report_id')->references('id')->on('reports')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checks');
+    }
+}
