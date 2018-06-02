59ef051f788c31780b2160f1ada40b6392af8f99
platinize <you@example.com>
2018-06-02 13:44:48 +0300

    add migration add studClass_id in reports

---
+++ database/migrations/2018_06_02_103948_add_student_class_id_to_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStudentClassIdToReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->integer('student_class_id')->unsigned()->nullable();
+            $table->foreign('student_class_id')->references('id')->on('students_classes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropColumn('student_class_id');
+        });
+    }
+}
