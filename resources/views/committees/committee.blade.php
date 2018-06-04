77a4148f2acc7122087e82473c60db9121c015a6
platinize <you@example.com>
2018-06-04 21:29:19 +0300

    value settings->address added to default value address in form createEvent

--- resources/views/committees/committee.blade.php
+++ resources/views/committees/committee.blade.php
@@ -139,54 +139,8 @@
                                          class="so-panel widget widget_buntington2-button-banner" data-index="5">
                                         <div class="panel-widget-style panel-widget-style-for-425-1-2-1">
                                             <div class="so-widget-buntington2-button-banner so-widget-buntington2-button-banner-base">
-                                                <h2 class="widget-title">Полезные ссылки</h2>
-                                                <a href="http://www.mon.gov.ua/"
-                                                   target="_self" class="button-banner-link">
-                                                    <div class="button-banner-wrapper" style="background-color: #5dca9d; ; text-align: left; padding: 20px 20px 20px 20px;">
-                                                        <div class="button-banner-text">
-                                                            <h2 class="button-banner-title" style="color: #f7f5de; font-size: 22px;"> Министерство образования и науки Украины</h2>
-                                                            <div class="button-banner-tagline" style="color: #FFFFFF;">Ссылка на сайт МОН </div>
-                                                        </div>
-                                                        <div class="dbb-hover-canvas"></div>
-                                                    </div>
-                                                </a>
-                                                <a href="http://testportal.gov.ua/"
-                                                   target="_self" class="button-banner-link" style="margin-top: 10px;">
-                                                    <div class="button-banner-wrapper" style="background-color: #b9becd; ; text-align: left; padding: 20px 20px 20px 20px;">
-                                                        <div class="button-banner-text">
-                                                            <h2 class="button-banner-title" style="color: #f7f5de; font-size: 22px;"> УЦОЯО</h2>
-                                                            <div class="button-banner-tagline"
-                                                                 style="color: #FFFFFF;">Ссылка на сайт УЦОЯО
-                                                            </div>
-                                                        </div>
-                                                        <div class="dbb-hover-canvas"></div>
-                                                    </div>
-                                                </a>
-                                            </div>
-                                            <div id="panel-425-1-0-1"
-                                                 class="so-panel widget widget_doublef-featured-gallery panel-last-child"
-                                                 data-index="2">
-                                                @isset($albums)
-                                                <div class="so-widget-doublef-featured-gallery so-widget-doublef-featured-gallery-base"><br/><br/>
-                                                    <a href="{{route('album.index')}}"><h5 class="doublef-gallery-photos-num"></h5><h2 class="widget-title">Избранная галлерея</h2></a>
-                                                    <div class="doublef-gallery-widget-wrap">
-                                                        <figure class="post-thumbnail">
-                                                            <a href="{{route('album.index')}}"
-                                                               title="School Photo Shots">
-                                                                <img width="1140" height="500"
-                                                                     src="http://buntington2.wpshow.me/wp-content/uploads/2014/06/14093140180_40cc891232_b.jpg"
-                                                                     class="attachment-full size-full wp-post-image" alt=""
-                                                                     srcset="http://buntington2.wpshow.me/wp-content/uploads/2014/06/14093140180_40cc891232_b.jpg 1140w, http://buntington2.wpshow.me/wp-content/uploads/2014/06/14093140180_40cc891232_b-600x263.jpg 600w"
-                                                                     sizes="(max-width: 1140px) 100vw, 1140px"/>
-                                                            </a>
-                                                        </figure>
-                                                        <div class="doublef-gallery-title-wrap">
-                                                            <h2 class="doublef-gallery-title">
-                                                            </h2>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                @endisset
+                                                <h2 class="widget-title">Отчёты</h2>
+
                                             </div>
                                         </div>
                                     </div>
