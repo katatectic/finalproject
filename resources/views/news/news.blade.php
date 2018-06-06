31872fa1a557df26e86dc55bfb87e92d49ac3716
katatectic <katatectic@gmail.com>
2018-06-06 13:48:11 +0300

    News view

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -27,29 +27,16 @@
             <a href="{{ route('oneCommittee',['id' => $committee->id]) }}">Комитет {{$classesNumbers()[$committee->id]}}-{{$committee->letter_class}} класса</a> /
         @endisset
         Новости
-    </div>
-    <form method="POST" action="{{route('article.choose')}}">
-        {{ csrf_field() }}
-        <select name="month">
-            @foreach (range(1,12) as $month)
-                <option value="{{$month}}">{{$monthNames[$month]}}</option>
-            @endforeach
-        </select>
-        <select name="year">
-            @foreach (range($thisYear,2000) as $year)
-                <option value="{{$year}}">{{$year}}</option>
-            @endforeach
-        </select>
-        <button type="submit" id="subbut" class="button">Найти</button>
-    </form>
+    </div>    
     @if (count($all) > 0)   
     <section class="news">
-        @foreach($all as $news)
+        
         <div id="site-to-top"><i class="fa fa-chevron-up fa-lg"></i></div>
         <div class="grid">
             <div id="primary" class="content-area grid__col grid__col--2-of-3 grid__col--m-1-of-1">    
                 <main id="main" class="site-main" role="main">
                     <div class="article-wrapper layout-sleek">
+                        @foreach($all as $news)
                         <article id="post-35" class="post-35 post type-post status-publish format-standard has-post-thumbnail hentry category-news tag-galleries tag-meetings tag-school" style="margin-top: 30px;">
                             <figure class="post-thumbnail">
                                 <a href="{{route('article',['id'=>$news->id])}}" title="{{ $news->title }}">
@@ -76,11 +63,69 @@
                                 <span class="screen-reader-text">Продолжить чтение  {{ $news->title }}</span>
                             </div>
                         </article>
+                        @endforeach
                     </div>
                 </main>
             </div>
-        </div>
-        @endforeach
+            <div id="primary" class="content-area grid__col grid__col--1-of-3 grid__col--m-1-of-1">
+                <h4 style="margin-top: 25px;">Выберите новости за месяц</h4>
+                <form method="POST" action="{{route('article.choose')}}">
+                    {{ csrf_field() }}
+                    <label for="month">Выберите месяц</label>
+                    <select name="month">
+                        @foreach (range(1,12) as $month)
+                        <option value="{{$month}}">{{$monthNames[$month]}}</option>
+                        @endforeach
+                    </select>
+                    <label for="year">Выберите год</label>
+                    <select name="year">
+                        @foreach (range($thisYear,2000) as $year)
+                        <option value="{{$year}}">{{$year}}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit" id="subbut" class="button">Найти</button>
+                </form>
+                <div class="so-widget-doublef-courses-search so-widget-doublef-courses-search-base">
+                    <h2 class="widget-title" style="margin-top: 20px;">Что-то будем искать?</h2>
+                    <form role="search" method="get" id="course-finder"
+                          class="search-form one-field-submit"
+                          action="{{route('search')}}">
+                        <label>
+                            <span class="screen-reader-text">Что-нибудь поищи...</span>
+                            <input type="text" class="search-field" placeholder="Поиск..." name="search"/>
+                            <input type="hidden" name="post_type" value="doublef-course"/>
+                        </label>
+                        {{ csrf_field() }}
+                        <input type="submit" class="search-submit" value="Искать"/>
+                    </form>
+                </div>
+                <div class="so-widget-buntington2-button-banner so-widget-buntington2-button-banner-base">
+                    <h2 class="widget-title" style="margin-top: 20px;">Полезные ссылки</h2>  
+                    <a href="http://www.mon.gov.ua/"
+                       target="_self" class="button-banner-link">
+                        <div class="button-banner-wrapper" style="background-color: #5dca9d; ; text-align: left; padding: 20px 20px 20px 20px;">
+                            <div class="button-banner-text">
+                                <h2 class="button-banner-title" style="color: #f7f5de; font-size: 22px;"> Министерство образования и науки Украины</h2>
+                                <div class="button-banner-tagline" style="color: #FFFFFF;">Ссылка на сайт МОН </div>
+                            </div>
+                            <div class="dbb-hover-canvas"></div>
+                        </div>
+                    </a>
+                    <a href="http://testportal.gov.ua/"
+                       target="_self" class="button-banner-link" style="margin-top: 10px;">
+                        <div class="button-banner-wrapper" style="background-color: #b9becd; ; text-align: left; padding: 20px 20px 20px 20px;">
+                            <div class="button-banner-text">
+                                <h2 class="button-banner-title" style="color: #f7f5de; font-size: 22px;"> УЦОЯО</h2>
+                                <div class="button-banner-tagline"
+                                     style="color: #FFFFFF;">Ссылка на сайт УЦОЯО
+                                </div>
+                            </div>
+                            <div class="dbb-hover-canvas"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>        
         {{$all->links()}}
     </section>
     @endif
